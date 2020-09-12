c4e0f45483831166ed62e7f6c024326ef17ce5bb
Lucas Menossi <lucasmenossi03@gmail.com>
2020-09-12 01:25:54 -0300

    Updates

---
+++ precache-manifest.5786dd0d3823825360056d5ae8ab48e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8f1b967909ca7228f6382af0a05946",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "3be41df7a93dacc67798",
+    "url": "/covid_tracker/static/css/main.b8b90d6b.chunk.css"
+  },
+  {
+    "revision": "ece9c86906838ea7b4aa",
+    "url": "/covid_tracker/static/js/2.1a2126f5.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid_tracker/static/js/2.1a2126f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be41df7a93dacc67798",
+    "url": "/covid_tracker/static/js/main.668f6f8a.chunk.js"
+  },
+  {
+    "revision": "a883e06c2ae183536200",
+    "url": "/covid_tracker/static/js/runtime-main.aed91c53.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
